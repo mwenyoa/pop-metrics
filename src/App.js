df606ca9ff71f7a72cc7cba4e91e53446819bb9f
Anthony Mwenyo <mwenyoanthony@gmail.com>
2022-06-21 01:14:18 +0200

    Define app pages or routes

--- src/App.js
+++ src/App.js
@@ -1,8 +1,23 @@
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Category from './components/Category';
+import Details from './components/Details';
+import Header from './components/Header';
 
 function App() {
   return (
-    <div className="App" />
+    <>
+
+      <Router>
+        <Header />
+        <main>
+          <Routes>
+            <Route path="/" element={<Category />} exact />
+            <Route path="/Details/:code" element={<Details />} exact />
+          </Routes>
+        </main>
+      </Router>
+    </>
   );
 }
 
