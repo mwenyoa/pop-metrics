510528c89c8a94b54ff0e2ec0bc4bfc97c0f88c4
Anthony Mwenyo <mwenyoanthony@gmail.com>
2022-06-23 21:30:21 +0200

    Write logic to display census data per country

--- src/components/Details.js
+++ src/components/Details.js
@@ -1,18 +1,49 @@
-import React from 'react';
+/* eslint-disable max-len */
+import { React, useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import { useParams } from 'react-router-dom';
+import { fetchPopulation } from '../redux/population/population';
 import style from './styles/CatDetails.module.css';
 
 const Details = () => {
-  const params = useParams();
+  const data = useSelector((state) => state.population);
+  const countries = data.population;
+
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    if (data.length === 0) {
+      dispatch(fetchPopulation());
+    }
+  }, []);
+
+  const country = useParams();
+
+  const mapped = countries?.filter((obj) => obj.country === country.code);
   return (
     <section className={style.catDetails}>
-      <h2 className={style.categoryname}>Census Details</h2>
-      <div className={style.detailMsg}>
-        <h3>
-          Zambian Population from 1961 - 2018
-        </h3>
-        <p>{params.code}</p>
-      </div>
+      {
+        mapped?.map((popInfo) => (
+          <div key={popInfo.code}>
+            <h2 className={style.categoryname}>
+              {popInfo.country}
+              <br />
+              1960 - 2018 Census
+            </h2>
+            <div className={style.Titles}>
+              <h3>YEAR</h3>
+              <h3>POPULATION</h3>
+            </div>
+            {popInfo.populationCounts.map((pop) => (
+              <li key={pop.year} className={style.metricsData}>
+                <p>{pop.year}</p>
+                <p>{pop.value}</p>
+              </li>
+            ))}
+            <div className={style.detailMsg} />
+          </div>
+        ))
+    }
     </section>
   );
 };
